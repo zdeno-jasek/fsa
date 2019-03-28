d06fd194c989219e6ad08ffaf8dbb66095d77e4d
zdeno-jasek <32713775+zdeno-jasek@users.noreply.github.com>
2019-03-28 13:45:07 +0100

    Vyhodenie implementacie

--- src/main/java/sk/posam/discussion/model/DiscussionMessageFactory.java
+++ src/main/java/sk/posam/discussion/model/DiscussionMessageFactory.java
@@ -6,26 +6,4 @@ import sk.posam.discussion.model.DiscussionMessageFactoryTest.NotifikaciaTest;
  */
 public final class DiscussionMessageFactory {
 
-	/**
-	 */
-	public static DiscussionMessage create(String name, String description, User user, DiscussionMessageType type, Course course, Notification notifikacia ){
-		return new DiscussionMessage(name, description, user, type, course);
-	}
-
-	private DiscussionMessageFactory(){
-	}
-
-	public static DiscussionMessage createQuestion(String name, String description, User user, Course course,
-			NotifikaciaTest notifikacia) {
-		return create(name, description, user, DiscussionMessageType.QUESTION, course, notifikacia);
-	}
-
-	public static DiscussionMessage createAnswer(DiscussionMessage otazka, String name, String description, User user,
-			Course course, NotifikaciaTest notifikacia) {
-		DiscussionMessage answer = create(name, description, user, DiscussionMessageType.ANSWER, course, notifikacia);
-		otazka.addChild(answer);
-		
-		return answer;
-	}
-
 }
